8014fda13a6e3eae6eb0d0e393a4bce4fc67a40b
AdnanKuntaru <majktr@gmail.com>
2018-10-19 06:00:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/a1stpluralsight/MyWorker.java
@@ -0,0 +1,11 @@
+package com.example.android.a1stpluralsight;
+
+/**
+ * Created by USER on 10/18/2018.
+ */
+
+public class MyWorker {
+    public static int doubleTheValue(int valuee){
+        return valuee * 2;
+    }
+}
